4d048f92d9286dbc245ff293d9fc4a6d523048b3
alan vergara <alan_vergara_p@hotmail.com>
2022-06-14 10:58:53 -0500

    ovies controller now allows to do múltiple word search with @@

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,19 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      # @movies = Movie.where(title: params[:query]) esta query trae la busqueda por palabra exacta.
+      # query_sql = "title ILIKE :query OR synopsis ILIKE :query" # se guarda en una variable la bísqueda de SQL
+      query_sql = "\
+      movies.title @@ :query \
+      OR movies.synopsis @@ :query \
+      OR directors.first_name @@ :query \
+      OR directors.last_name @@ :query \
+      "
+      # con POSGRESQL se sustituye el ILIKE por @@ para poder realizar consultas de palábras múltiples.
+      @movies = Movie.joins(:director).where(query_sql, query: "%#{params[:query]}%") # esta query trae todas las películas que contienen
+      # una parte de la búsqueda en el título. "I" es de insensitive. % funciona como comodín, puede tener adelante o atrás.
+    else
+      @movies = Movie.all
+    end
   end
 end
