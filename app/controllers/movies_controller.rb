a6c37e32a45d3fe70074852fb25d289ecbaff666
alan vergara <alan_vergara_p@hotmail.com>
2022-06-14 14:13:41 -0500

    PgSearch multisearch works on server and shows both movies and Tv shows, but no @movies, ElasticSearch not installed yet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,15 +3,18 @@ class MoviesController < ApplicationController
     if params[:query].present?
       # @movies = Movie.where(title: params[:query]) esta query trae la busqueda por palabra exacta.
       # query_sql = "title ILIKE :query OR synopsis ILIKE :query" # se guarda en una variable la bísqueda de SQL
-      query_sql = "\
-      movies.title @@ :query \
-      OR movies.synopsis @@ :query \
-      OR directors.first_name @@ :query \
-      OR directors.last_name @@ :query \
-      "
-      # con POSGRESQL se sustituye el ILIKE por @@ para poder realizar consultas de palábras múltiples.
-      @movies = Movie.joins(:director).where(query_sql, query: "%#{params[:query]}%") # esta query trae todas las películas que contienen
+      # query_sql = "\
+      # movies.title @@ :query \
+      # OR movies.synopsis @@ :query \
+      # OR directors.first_name @@ :query \
+      # OR directors.last_name @@ :query \
+      # "
+      # # con POSGRESQL se sustituye el ILIKE por @@ para poder realizar consultas de palábras múltiples.
+      # @movies = Movie.joins(:director).where(query_sql, query: "%#{params[:query]}%") # esta query trae todas las películas que contienen
       # una parte de la búsqueda en el título. "I" es de insensitive. % funciona como comodín, puede tener adelante o atrás.
+      # @movies = Movie.global_search(params[:query]) # metodo que jalamos del modelo
+      # una vez que ejecutamos el PgSearch la línea de arriba no sirve más
+      @results = PgSearch.multisearch(params[:query]) # nombre del método en el modelo Movies
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  include PgSearch::Model
+  multisearchable against: %i[title synopsis]
+  # pg_search_scope :global_search, # nombre del método en el controlador
+  #   against: %i[title synopsis], # en qué columnas queremos buscar
+  #   associated_against: { # permite realizar búsquedas por asociaciones en otras tablas
+  #     director: %i[first_name last_name]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
